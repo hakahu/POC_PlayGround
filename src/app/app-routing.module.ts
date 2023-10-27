21e408ad6f5cbcc4ef74444b3b9ad2416aa9675c
Timo Schmitt <hakahugaming@gmail.com>
2023-10-27 20:54:03 +0200

    Added navbar component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    component: NavbarComponent
+  },
+
+  // {
+  //   path: 'home',
+  //   component: LandingPageComponent
+  // },
+  {
+    path: '**',
+    redirectTo: ''
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
