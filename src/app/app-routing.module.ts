c0e11a5f7c83402e1189b9cc6b11a19b52f211bf
Alex Wolf <a.wolf@eikona.de>
2024-01-13 00:07:19 +0100

    wip

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 // import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
+import { PaymentComponent } from './payment/payment.component';
+import { OrderingComponent } from './ordering/ordering.component';
 
 const routes: Routes = [
 
@@ -12,6 +14,14 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'payment',
+    component: PaymentComponent
+  },
+  {
+    path: 'ordering',
+    component: OrderingComponent
   },
   {
     path: '**',
