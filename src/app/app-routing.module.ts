b82e8fef5dc76dee49dad5f42b3e59e8e4becab0
Alex Wolf <a.wolf@eikona.de>
2024-01-12 22:16:15 +0100

    wip

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
+// import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
 
   {
     path: '',
-    component: NavbarComponent
+    component: LoginComponent
   },
   {
     path: 'login',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+// import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    LoginComponent
+    // NavbarComponent,
+    LoginComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
