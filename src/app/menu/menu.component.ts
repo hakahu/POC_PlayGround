6acededcf2545886027cd6024350fdfd1c4396ba
Alex Wolf <a.wolf@eikona.de>
2024-02-23 23:34:11 +0100

    added product and login service, worked on menu component

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { MenuItem } from '../../models/models';
+import { Dishes, MenuItem } from '../../models/models';
+import { ProductService } from '../services/product.service';
 
 
 @Component({
@@ -9,22 +10,63 @@ import { MenuItem } from '../../models/models';
 })
 export class MenuComponent implements OnInit {
   
-  
-  menu: MenuItem[] = [
-      new MenuItem("Alles"),
-      new MenuItem("Vorspeise"),
-      new MenuItem("Hauptgericht"),
-      new MenuItem("Desserts"),
-      new MenuItem("Getränke"),
-      // ... Add more items as needed
-  ];
-  
-  constructor() {
+  constructor(productService: ProductService) {
     
   }
 
   ngOnInit() {
     
   }
+
+  menu: MenuItem[] = [
+      new MenuItem("Alles"),
+      new MenuItem("Vorspeise"),
+      new MenuItem("Hauptgericht"),
+      new MenuItem("Desserts"),
+      new MenuItem("Getränke"),
+  ];
+  
+  dishes: Dishes[] = [
+  {
+    ingredients: ["ingredient1", "ingredient2", "ingredient3"],
+    description: "Lasagne",
+    price: "$10.99",
+    link: "../../assets/food/lasagne.jpg",
+    alt: "Lasagne alt text",
+    name: "Lasagne"
+  },
+  {
+    ingredients: ["ingredient1", "ingredient2", "ingredient3"],
+    description: "Pelimeschki",
+    price: "$8.99",
+    link: "../../assets/food/pelmeni.jpg",
+    alt: "Pelimeschki alt text",
+    name: "Pelimeschki"
+  },
+  {
+    ingredients: ["ingredient1", "ingredient2", "ingredient3"],
+    description: "Borsch",
+    price: "$12.99",
+    link: "../../assets/food/borschtsch.jpg",
+    alt: "Borsch alt text",
+    name: "Borsch"
+  },
+  {
+    ingredients: ["ingredient1", "ingredient2", "ingredient3"],
+    description: "Spaghetti Carbonara",
+    price: "$14.99",
+    link: "../../assets/food/carbonara.jpg",
+    alt: "Spaghetti Carbonara alt text",
+    name: "Lasagne"
+  },
+  {
+    ingredients: ["ingredient1", "ingredient2", "ingredient3"],
+    description: "Nodelbok dlei dleißig",
+    price: "$3.30",
+    link: "../../assets/food/nudelbox.jpg",
+    alt: "Nodelbok dlei dleißig alt text",
+    name: "Nodelbok dlei dleißig"
+  },
+  ];
 
 }
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,50 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Dishes } from 'src/models/models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private http: HttpClient) { 
+
+  }
+
+  getAllProducts(){
+    this.http.get<Dishes>('https://reqres.in/api/posts').subscribe(response => {
+        console.log(response);
+    })
+  }
+
+  getProductById(){
+    this.http.get<Dishes>('https://reqres.in/api/posts').subscribe(response => {
+        console.log(response);
+    })
+  }
+
+  addProduct(id:string){
+    this.http.post<Dishes>('https://reqres.in/api/posts', { id: id }).subscribe(response => {
+        console.log(response);
+    })
+  }
+
+  updateProduct(id:string){
+    this.http.put<Dishes>('https://reqres.in/api/posts', { id: id }).subscribe(response => {
+        console.log(response);
+    })
+  }
+
+  removeProduct(id:string){
+    this.http.delete('https://jsonplaceholder.typicode.com/invalid-url')
+        .subscribe({
+            next: response => {
+              console.log(response);
+            },
+            error: error => {
+                console.error('There was an error!', error.message);
+            }
+        });
+  }
+
+}
