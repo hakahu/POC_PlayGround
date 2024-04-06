6e4ad5f544f164ade1f42a13a81c76632bf71aa7
Alex Wolf <alexwolf822@yahoo.de>
2024-04-06 21:36:58 +0200

    wip

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit{
 
-  ngOnInit()
-  {
-    
+  subtitles: string[] = ["Die besten Restaurants.", "Die besten Angebote.", "In deiner Nähe."];
+  currentIndex: number = 0;
+  currentSubtitle: string = '';
+  animateSubtitle: boolean = false;
+
+  ngOnInit() {
+    this.changeSubtitle();
   }
+
+  changeSubtitle() {
+    this.animateSubtitle = false;
+    setTimeout(() => {
+        this.currentSubtitle = this.subtitles[this.currentIndex];
+        this.animateSubtitle = true;
+        this.currentIndex = (this.currentIndex + 1) % this.subtitles.length;
+    }, 1);
+    setInterval(() => {
+        this.animateSubtitle = false;
+        setTimeout(() => {
+            this.currentSubtitle = this.subtitles[this.currentIndex];
+            this.animateSubtitle = true;
+            this.currentIndex = (this.currentIndex + 1) % this.subtitles.length;
+        }, 1500);
+    }, 1500);
+}
   
 }
