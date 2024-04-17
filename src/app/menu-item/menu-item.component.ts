b65f58ef4052e820e0b5610dd4f2e5ad4b311cd1
Capsova <sovamax19@gmail.com>
2024-04-17 20:49:45 +0200

    added minimal Style for QR Component and changed Menu Item Component to Modal dialog

--- src/app/menu-item/menu-item.component.ts
+++ src/app/menu-item/menu-item.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu-item.component.css']
 })
 export class MenuItemComponent {
+  showModal: boolean = false; // Variable zur Steuerung der Modal-Anzeige
 
+  constructor() {}
+
+  // Methode zum Öffnen des Modals
+  openModal() {
+    this.showModal = true;
+  }
+
+  // Methode zum Schließen des Modals
+  closeModal() {
+    this.showModal = false;
+  }
 }
